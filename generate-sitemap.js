78ae3092a06f9b88ea6819b40d2865a7e47ab372
alxkrrgn <alxkrgn@hotmail.com>
2024-12-18 18:35:41 -0800

    Added sitemap corrected with my domain and pages corrected with priorites

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -6,12 +6,12 @@ const { createWriteStream } = require('fs');
 
   sitemap.write({ url: '/', changefreq: 'daily', priority: 1.0 });
   sitemap.write({ url: '/strategies', changefreq: 'weekly', priority: 0.8 });
-  sitemap.write({ url: '/contact', changefreq: 'weekly', priority: 0.7 });
-  sitemap.write({ url: '/getstarted', changefreq: 'weekly', priority: 0.7 });
-  sitemap.write({ url: '/stocks', changefreq: 'weekly', priority: 0.7 });
-  sitemap.write({ url: '/stock-options', changefreq: 'weekly', priority: 0.7 });
-  sitemap.write({ url: '/login', changefreq: 'weekly', priority: 0.7 });
-  sitemap.write({ url: '/login', changefreq: 'weekly', priority: 0.7 });
+  sitemap.write({ url: '/contact', changefreq: 'weekly', priority: 0.6 });
+  sitemap.write({ url: '/getstarted', changefreq: 'weekly', priority: 0.4 });
+  sitemap.write({ url: '/about', changefreq: 'weekly', priority: 0.3 });
+  sitemap.write({ url: '/stock-options', changefreq: 'weekly', priority: 0.5 });
+  sitemap.write({ url: '/login', changefreq: 'weekly', priority: 0.9 });
+  sitemap.write({ url: '/register', changefreq: 'weekly', priority: 0.7 });
 
   sitemap.end();
 
